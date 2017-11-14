99c496eab2f86959a233bc771351e75b22e6cf45
Bob Dietrich <bob.dietrich@rigado.com>
2017-11-13 17:54:27 -0800

    Add code to close S3 objects with repository objects are deleted.

--- hawkbit-extension-artifact-repository-s3/src/main/java/org/eclipse/hawkbit/artifact/repository/S3Repository.java
+++ hawkbit-extension-artifact-repository-s3/src/main/java/org/eclipse/hawkbit/artifact/repository/S3Repository.java
@@ -167,6 +167,8 @@ public class S3Repository implements ArtifactRepository {
         final String key = objectKey(tenant, sha1Hash);
 
         LOG.info("Deleting S3 object from bucket {} and key {}", s3Properties.getBucketName(), key);
+        final S3Object s3Object = amazonS3.getObject(s3Properties.getBucketName(), key);
+        closeS3Object(s3Object);
         amazonS3.deleteObject(new DeleteObjectRequest(s3Properties.getBucketName(), key));
     }
 
@@ -242,7 +244,9 @@ public class S3Repository implements ArtifactRepository {
         ObjectListing objects = amazonS3.listObjects(s3Properties.getBucketName(), folder + "/");
         do {
             for (final S3ObjectSummary objectSummary : objects.getObjectSummaries()) {
-                amazonS3.deleteObject(s3Properties.getBucketName(), objectSummary.getKey());
+                final S3Object s3Object = amazonS3.getObject(s3Properties.getBucketName(), objectSummary.getKey());
+                closeS3Object(s3Object);
+            	amazonS3.deleteObject(s3Properties.getBucketName(), objectSummary.getKey());
             }
             objects = amazonS3.listNextBatchOfObjects(objects);
         } while (objects.isTruncated());
@@ -251,5 +255,16 @@ public class S3Repository implements ArtifactRepository {
 
     private static String sanitizeTenant(final String tenant) {
         return tenant.trim().toUpperCase();
+    }
+    
+    private void closeS3Object(final S3Object s3Object) {
+        if (s3Object != null) {
+        	try {
+        		s3Object.close();
+        	} catch (IOException ex) {
+        		// Housekeeping did not succeed, but we we're about to delete anyway.
+        		// Might already be closed.
+        	}
+        }
     }
 }
