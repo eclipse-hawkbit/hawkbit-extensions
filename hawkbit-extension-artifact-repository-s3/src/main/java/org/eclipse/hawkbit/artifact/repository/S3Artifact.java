aeced15924424da6475175a25bd38439a7bc8158
Michael Herdt <michael.herdt@bosch.io>
2021-03-19 13:23:31 +0100

    Introduce a wrapper for the S3ObjectInputStream to abort the connection before closing it.
    Signed-off-by: Michael Herdt <Michael.Herdt@bosch.io>

--- hawkbit-extension-artifact-repository-s3/src/main/java/org/eclipse/hawkbit/artifact/repository/S3Artifact.java
+++ hawkbit-extension-artifact-repository-s3/src/main/java/org/eclipse/hawkbit/artifact/repository/S3Artifact.java
@@ -8,11 +8,14 @@
  */
 package org.eclipse.hawkbit.artifact.repository;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.google.common.io.BaseEncoding;
+import org.apache.http.client.methods.HttpRequestBase;
 import org.eclipse.hawkbit.artifact.repository.model.AbstractDbArtifact;
 import org.eclipse.hawkbit.artifact.repository.model.DbArtifactHash;
 import org.slf4j.Logger;
@@ -33,13 +36,14 @@ public final class S3Artifact extends AbstractDbArtifact {
     private final S3RepositoryProperties s3Properties;
     private final String key;
     private S3Object s3Object;
-
+    private WrappedS3InputStream s3InputStream;
 
     private S3Artifact(final S3Object s3Object, final AmazonS3 amazonS3, final S3RepositoryProperties s3Properties,
             final String key, final String artifactId, final DbArtifactHash hashes, final Long size,
             final String contentType) {
         this(amazonS3, s3Properties, key, artifactId, hashes, size, contentType);
         this.s3Object = s3Object;
+        this.s3InputStream = WrappedS3InputStream.wrap(s3Object.getObjectContent());
     }
 
     private S3Artifact(final AmazonS3 amazonS3, final S3RepositoryProperties s3Properties, final String key,
@@ -107,7 +111,7 @@ public final class S3Artifact extends AbstractDbArtifact {
      *
      * @return result of {@link AmazonS3}#doesObjectExist
      */
-    public boolean exists(){
+    public boolean exists() {
         return amazonS3.doesObjectExist(s3Properties.getBucketName(), key);
     }
 
@@ -120,21 +124,16 @@ public final class S3Artifact extends AbstractDbArtifact {
     @Override
     public InputStream getFileInputStream() {
         LOG.debug("Get file input stream for s3 object with key {}", key);
-        return getS3Object().getObjectContent();
+        refreshS3ObjectIfNeeded();
+        return s3InputStream;
     }
-
-    @Override
-    public void abortIfNeeded() {
-        LOG.info("Abort requested for artifact input stream with key {}", key);
-        getS3Object().getObjectContent().abort();
-    }
-
-    private S3Object getS3Object() {
-        if (s3Object == null) {
-            LOG.debug("Initialize S3Object in bucket {} with key {}", s3Properties.getBucketName(), key);
+    
+    private void refreshS3ObjectIfNeeded() {
+        if (s3Object == null || s3InputStream == null) {
+            LOG.info("Initialize S3Object in bucket {} with key {}", s3Properties.getBucketName(), key);
             s3Object = amazonS3.getObject(s3Properties.getBucketName(), key);
+            s3InputStream = WrappedS3InputStream.wrap(s3Object.getObjectContent());
         }
-        return s3Object;
     }
 
     private static S3Object getS3ObjectOrThrowException(AmazonS3 amazonS3, String bucketName, String key)
@@ -157,4 +156,34 @@ public final class S3Artifact extends AbstractDbArtifact {
         return etag.trim().replaceAll("[^A-Za-z0-9+/=]", "");
     }
 
+    /**
+     * Wrapper to abort the http request of the S3 input stream before closing it
+     */
+    static class WrappedS3InputStream extends S3ObjectInputStream {
+
+        /**
+         * Constructor
+         */
+        private WrappedS3InputStream(InputStream in, HttpRequestBase httpRequest) {
+            super(in, httpRequest);
+        }
+
+        /**
+         * Wrap an input stream of type {@link S3ObjectInputStream} to abort a
+         * connection before closing the stream
+         * 
+         * @param inputStream
+         *            the {@link S3ObjectInputStream}
+         * @return an instance of {@link WrappedS3InputStream}
+         */
+        public static WrappedS3InputStream wrap(final S3ObjectInputStream inputStream) {
+            return new WrappedS3InputStream(inputStream, inputStream.getHttpRequest());
+        }
+
+        @Override
+        public void close() throws IOException {
+            super.abort();
+            super.close();
+        }
+    }
 }
